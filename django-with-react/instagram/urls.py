9421f20b9a18771b1a5824d76edd8cec24bdc666
Changmin Lee <111254126+DiosAmor@users.noreply.github.com>
2023-11-21 17:52:35 +0900

    django-v4

--- django-with-react/instagram/urls.py
+++ django-with-react/instagram/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     path('',views.post_list),
     path('<int:pk>/',views.post_detail),
     # re_path
+    path('archives/<year:year>/',views.archives_year),
 ]
--- django-with-react/instagram/views.py
+++ django-with-react/instagram/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView
 from django.http import HttpResponse, HttpRequest, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 post_list = ListView.as_view(model=Post)
@@ -18,5 +18,14 @@ post_list = ListView.as_view(model=Post)
 #     })
     
 def post_detail(request: HttpRequest,pk:int)->HttpResponse:
-    response = HttpResponse()
-    return response
+    post = get_object_or_404(Post,pk=pk)
+    # try:
+    #     post = Post.objects.get(pk=pk)
+    # except Post.DoesNotExist:
+    #     raise Http404
+    return render(request,'instagram/post_detail.html',{
+        'post':post,
+    })
+
+def archives_year(request,year):
+    return HttpResponse(f"{year}년 archives")
