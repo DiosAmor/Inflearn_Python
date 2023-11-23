36be861527ef21cfa540b9a86cfd753fc23675c0
Changmin Lee <111254126+DiosAmor@users.noreply.github.com>
2023-11-23 18:56:13 +0900

    django-v6

--- django-with-react/instagram/views.py
+++ django-with-react/instagram/views.py
@@ -1,11 +1,20 @@
 from typing import Any
 from django.db import models
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView
 from django.http import HttpResponse, HttpRequest, Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
-post_list = ListView.as_view(model=Post)
+# post_list = login_required(ListView.as_view(model=Post,paginate_by=10))
+
+@method_decorator(login_required, name = 'dispatch')
+class PostListView(ListView):
+    model = Post
+    paginate_by=10
+
+post_list = PostListView.as_view()
 
 # # Create your views here.
 # def post_list(request):
