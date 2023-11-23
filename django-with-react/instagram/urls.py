10668e78a652cbc133502f6043ef8bdafebb23f3
Changmin Lee <111254126+DiosAmor@users.noreply.github.com>
2023-11-23 18:43:39 +0900

    django-v5

--- django-with-react/instagram/urls.py
+++ django-with-react/instagram/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path, re_path
+from django.urls import path, re_path,  register_converter
 
 from . import views
+from .converters import YearConverter, MonthConverter, DayConverter
+
+register_converter(YearConverter, 'year')
+register_converter(MonthConverter, 'month')
+register_converter(DayConverter, 'day')
+
+
+app_name = 'instagram'  # URL Reverse에서 namespace역할을 하게 됩니다.
 
 urlpatterns=[
     path('',views.post_list),
--- django-with-react/instagram/views.py
+++ django-with-react/instagram/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView
+from typing import Any
+from django.db import models
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse, HttpRequest, Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Post
@@ -17,15 +19,32 @@ post_list = ListView.as_view(model=Post)
 #         'q':q,
 #     })
     
-def post_detail(request: HttpRequest,pk:int)->HttpResponse:
-    post = get_object_or_404(Post,pk=pk)
-    # try:
-    #     post = Post.objects.get(pk=pk)
-    # except Post.DoesNotExist:
-    #     raise Http404
-    return render(request,'instagram/post_detail.html',{
-        'post':post,
-    })
+# def post_detail(request: HttpRequest,pk:int)->HttpResponse:
+#     post = get_object_or_404(Post,pk=pk)
+#     # try:
+#     #     post = Post.objects.get(pk=pk)
+#     # except Post.DoesNotExist:
+#     #     raise Http404
+#     return render(request,'instagram/post_detail.html',{
+#         'post':post,
+#     })
+
+# post_detail = DetailView.as_view(
+#     model=Post,
+#     queryset=Post.objects.filter(is_public=True)
+# )
+
+class PostDetailView(DetailView):
+    model = Post
+    # queryset = Post.objects.filter(is_public = True)
+    
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if not self.request.user.is_authenticated:
+            qs = qs.filter(is_public=True)
+        return qs
+
+post_detail = PostDetailView.as_view()
 
 def archives_year(request,year):
     return HttpResponse(f"{year}년 archives")
